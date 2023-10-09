71230b9db98ca9ee08fdca62c5019337711c8a7a
Almightyjuju <alexanderpizzati11@gmail.com>
2023-10-09 12:49:59 -0500

    create home html and css basic structure

---
+++ javascript.js
@@ -0,0 +1,1 @@
+const message = "Hello, My name is Junior Pizzati. I am a student at Fullstack Academy learning to become a sofware developer. This is my personal site it's about who I am, what I am about, and what I have accomplished"
