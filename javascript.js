d9ce1c5bd55e1497d058bb5f2135683f6aa83ae7
Almightyjuju <alexanderpizzati11@gmail.com>
2023-10-11 16:42:16 -0500

    fixed css

--- javascript.js
+++ javascript.js
@@ -1,1 +1,4 @@
-const message = "Hello, My name is Junior Pizzati. I am a student at Fullstack Academy learning to become a sofware developer. This is my personal site it's about who I am, what I am about, and what I have accomplished"
+console.log("Welcome to Junior's Personal Website")
+
+
+alert ("Hello, My name is Junior Pizzati. I am a student at Fullstack Academy learning to become a sofware developer. This is my personal site it's about who I am, what I am about, and what I have accomplished")
