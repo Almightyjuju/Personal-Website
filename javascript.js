60be4017cbb3ef4212f87d4e066366eb76740f19
Almightyjuju <alexanderpizzati11@gmail.com>
2023-10-15 12:18:35 -0500

    working on javascript file

--- javascript.js
+++ javascript.js
@@ -1,4 +1,13 @@
-console.log("Welcome to Junior's Personal Website")
+let greetings = "Welcome to Junior's Personal Website"
+
+console.log(greetings)
+
+let includes = "HTML, CSS, Javascript files"
+
+console.log("My personal site is made up of  " + includes + "!")
+
+alert("Hello, My name is Junior Pizzati. I am a student at Fullstack Academy learning to become a sofware developer. This is my personal site it's about who I am, what I am about, and what I have accomplished")
 
 
-alert ("Hello, My name is Junior Pizzati. I am a student at Fullstack Academy learning to become a sofware developer. This is my personal site it's about who I am, what I am about, and what I have accomplished")
+
+let 
