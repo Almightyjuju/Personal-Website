263bd1e9507453d831184ddd8ec778de5a21bc55
Almightyjuju <alexanderpizzati11@gmail.com>
2023-10-17 15:35:14 -0500

    made some corrections to javascript

--- javascript.js
+++ javascript.js
@@ -1,16 +1,14 @@
 const jr = "Junior";
-console.log("Hello, my name is" + jr + ".")
+console.log("Hello, my name is " + jr + ".");
 
+let greetings = "Welcome to Junior's Personal Website";
 
-let greetings = "Welcome to Junior's Personal Website"
+console.log(greetings);
 
-console.log(greetings)
+let includes = "HTML, CSS, Javascript files";
 
-let includes = "HTML, CSS, Javascript files"
+console.log("My personal site is made up of " + includes + "!");
 
-console.log("My personal site is made up of  " + includes + "!")
-
-alert("Hello, My name is Junior Pizzati. I am a student at Fullstack Academy learning to become a sofware developer. This is my personal site it's about who I am, what I am about, and what I have accomplished")
-
-
-
+alert(
+  "Hello, My name is Junior Pizzati. I am a student at Fullstack Academy learning to become a sofware developer. This is my personal site it's about who I am, what I am about, and what I have accomplished"
+);
